a028133bbe986ef43186799372373cd57fe5c76c
irvingrivas <irving0594@gmail.com>
2018-11-04 21:22:24 -0700

    Finalized HW

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Heart Stopper');
+INSERT INTO burgers (burger_name) VALUES ('Cholestorol King');
+INSERT INTO burgers (burger_name) VALUES ('Double Meat');
+INSERT INTO burgers (burger_name) VALUES ('Vegetarian');
